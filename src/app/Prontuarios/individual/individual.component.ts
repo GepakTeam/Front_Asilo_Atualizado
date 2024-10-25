1f9da0956fc64b938e06227b403d32896a038a1c
Kessy Ramos <kessyellenramos@gmail.com>
2024-10-25 08:03:28 -0300

    MudançaListaInd

--- src/app/Prontuarios/individual/individual.component.ts
+++ src/app/Prontuarios/individual/individual.component.ts
@@ -43,7 +43,7 @@ export const MY_DATE_FORMATS: MatDateFormats = {
         MatNativeDateModule,
         RouterLink,
         NavbarComponent,
-        FormsModule
+        FormsModule,
     ]
 })
 
